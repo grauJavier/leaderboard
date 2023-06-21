d1bdb693bf081e90a91093f7b0800dfb3e8a06a7
Javier Grau <javiergrau.freelance@gmail.com>
2023-06-20 23:43:07 -0400

    Add event listener to 'add' & 'remove' buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './styles.css';
+import load from './modules/load.js';
+import renderScores from './modules/renderScores.js';
+import addScore from './modules/addScore.js';
+import refresh from './modules/refresh.js';
+import emptyMsg from './modules/emptyMsg.js';
+
+load();
+
+const addScoreForm = document.querySelector('#add-score__form');
+const nameInput = document.querySelector('#add-score__name-input');
+const scoreInput = document.querySelector('#add-score__score-input');
+
+addScoreForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  emptyMsg(true);
+  renderScores(nameInput.value, scoreInput.value);
+  addScore(nameInput.value, scoreInput.value);
+  addScoreForm.reset();
+});
+
+const refreshBtn = document.querySelector('#recent-scores__refresh-btn');
+
+refreshBtn.addEventListener('click', () => {
+  refresh();
+});
